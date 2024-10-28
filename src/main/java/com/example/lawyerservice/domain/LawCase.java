164187c3a12bf546449f6c21788d108b370aa04b
PiotrHic <piotrhic@gmail.com>
2024-10-28 14:44:35 +0100

    Create LawCase Class in domain package. Closes #11.

---
+++ src/main/java/com/example/lawyerservice/domain/LawCase.java
@@ -0,0 +1,18 @@
+package com.example.lawyerservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LawCase {
+
+    private Integer id;
+    private String name;
+    private Integer lawClientId;
+    private LawClient lawClient;
+}
