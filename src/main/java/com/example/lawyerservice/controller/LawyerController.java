93d2f8549752e6bb4de8d11574296fa4ad912f51
PiotrHic <piotrhic@gmail.com>
2024-10-28 20:57:56 +0100

    Implement Logger in Lawyer Controller. Closes #23.

--- src/main/java/com/example/lawyerservice/controller/LawyerController.java
+++ src/main/java/com/example/lawyerservice/controller/LawyerController.java
@@ -4,6 +4,8 @@ package com.example.lawyerservice.controller;
 import com.example.lawyerservice.domain.Lawyer;
 import com.example.lawyerservice.service.LawyerService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,9 @@ import java.util.List;
 @RequestMapping("/api/lawyer/")
 public class LawyerController {
 
+    private static final Logger LOGGER
+            = LoggerFactory.getLogger(LawyerController.class);
+
     private final LawyerService lawyerService;
 
     private final String NUMBER_PATH = "{lawyerId}";
@@ -22,7 +27,9 @@ public class LawyerController {
 
     @PostMapping
     ResponseEntity<Lawyer> createLawyer(@RequestBody Lawyer lawyer){
-        return new ResponseEntity<>(lawyerService.addLawyer(lawyer),
+        Lawyer added = lawyerService.addLawyer(lawyer);
+        LOGGER.info("Lawyer add: {}", added.toString());
+        return new ResponseEntity<>(lawyerService.addLawyer(added),
                 HttpStatus.valueOf(201));
     }
 
@@ -31,12 +38,14 @@ public class LawyerController {
             , @RequestBody Lawyer lawyer) {
         Lawyer updated = lawyerService.updateLawyer(lawyerId,
                 lawyer);
+        LOGGER.info("Lawyer updated: {}", updated.toString());
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @GetMapping(NUMBER_PATH)
     ResponseEntity <Lawyer> getLawyerById(@PathVariable(PATH_VARIABLE_PATH) Integer lawyerId) {
         Lawyer founded = lawyerService.getLawyerByID(lawyerId);
+        LOGGER.info("Lawyer founded: {}", founded.toString());
         return new ResponseEntity<>(founded, HttpStatus.OK);
     }
 
@@ -44,18 +53,22 @@ public class LawyerController {
     ResponseEntity <List<Lawyer>> getAllLawyers(){
         List<Lawyer> lawyers = lawyerService
                 .getAllLawyers();
+
+        LOGGER.info("All lawyers were founded!");
         return new ResponseEntity<>(lawyers, HttpStatus.OK);
     }
 
     @DeleteMapping(NUMBER_PATH)
     ResponseEntity <String> deleteLawyerById(@PathVariable(PATH_VARIABLE_PATH) Integer lawyerId){
         String deleted = lawyerService.deleteById(lawyerId);
+        LOGGER.info("Lawyer founded: {}", deleted.toString());
         return new ResponseEntity<>(deleted, HttpStatus.OK);
     }
 
     @DeleteMapping()
     ResponseEntity <String> deleteAllDancers(){
         lawyerService.deleteAll();
+        LOGGER.info("Database is empty");
         return new ResponseEntity<>("Database is empty", HttpStatus.OK);
     }
 
