5472fba836606f69049f1903cc7374331497ac65
PiotrHic <piotrhic@gmail.com>
2024-10-30 12:41:59 +0100

    Create LawClient Service Implementation. Closes #63.

---
+++ src/main/java/com/example/lawclientservice/service/LawClientServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.lawclientservice.service;
+
+
+import com.example.lawclientservice.domain.LawClient;
+import com.example.lawclientservice.repository.InMemoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class LawClientServiceImpl implements LawClientService {
+
+    private final InMemoryRepository inMemoryRepository;
+    @Override
+    public LawClient createLawClient(LawClient LawClient) {
+        return inMemoryRepository.addLawClient(LawClient);
+    }
+
+    @Override
+    public LawClient getLawClient(Integer lawClientId) {
+        return inMemoryRepository.getLawClientByID(lawClientId);
+    }
+
+    @Override
+    public List<LawClient> getAllLawClients() {
+
+        return inMemoryRepository.getAllLawClients();
+    }
+
+    @Override
+    public LawClient updateLawClient(Integer lawClientId, LawClient lawClient) {
+        return inMemoryRepository.updateLawClient(lawClientId,lawClient);
+    }
+
+    @Override
+    public String deleteLawClientByID(Integer lawClientId) {
+        return inMemoryRepository.deleteById(lawClientId);
+    }
+
+    @Override
+    public String deleteAllLawClients() {
+        return inMemoryRepository.deleteAll();
+    }
+
+    @Override
+    public LawClient getLawClientForLawCase(Integer lawClientId) {
+        return inMemoryRepository.getLawClientByID(lawClientId);
+    }
+
+}
