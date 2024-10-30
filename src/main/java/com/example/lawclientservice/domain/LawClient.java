9ae7bfd46fd66cc4d9213822d424bc36bf2067f4
PiotrHic <piotrhic@gmail.com>
2024-10-30 11:33:19 +0100

    Create LawClient Class. Closes #53.

---
+++ src/main/java/com/example/lawclientservice/domain/LawClient.java
@@ -0,0 +1,15 @@
+package com.example.lawclientservice.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LawClient {
+
+    private Integer id;
+    private String name;
+
+}
