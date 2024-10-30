3afa5cb87af268f59625b3b07e1201e61e9e7dea
PiotrHic <piotrhic@gmail.com>
2024-10-30 15:57:26 +0100

    Implement LawClientNotFoundException. Closes #70.

--- src/main/java/com/example/lawcaseservice/service/LawCaseServiceImpl.java
+++ src/main/java/com/example/lawcaseservice/service/LawCaseServiceImpl.java
@@ -3,8 +3,12 @@ package com.example.lawcaseservice.service;
 
 
 import com.example.lawcaseservice.domain.LawCase;
+import com.example.lawcaseservice.domain.LawClient;
+import com.example.lawcaseservice.exception.LawCaseNotFoundException;
 import com.example.lawcaseservice.repository.InMemoryRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +18,9 @@ import java.util.Set;
 @Service
 public class LawCaseServiceImpl implements LawCaseService {
 
+    private static final Logger LOGGER
+            = LoggerFactory.getLogger(LawCaseServiceImpl.class);
+
     private final InMemoryRepository inMemoryRepository;
     @Override
     public LawCase createLawCase(LawCase LawCase) {
@@ -21,7 +28,25 @@ public class LawCaseServiceImpl implements LawCaseService {
     }
 
     @Override
+    public LawCase updateLawCase(Integer id, LawCase LawCase) {
+        try{
+            LawCase founded = inMemoryRepository.getLawCaseByID(id);
+        } catch (Exception e){
+            LOGGER.info("LawCase not found! id : {}", id.toString());
+            throw new LawCaseNotFoundException("LawCase with id: " + id + " was not found!");
+        }
+        return inMemoryRepository.updateLawCase(id,LawCase);
+    }
+
+    @Override
     public LawCase getLawCase(Integer id) {
+        try{
+            LawCase founded = inMemoryRepository.getLawCaseByID(id);
+        } catch (Exception e){
+            LOGGER.info("LawCase not found! id : {}", id.toString());
+            throw new LawCaseNotFoundException("LawCase with id: " + id + " was not found!");
+        }
+
         return inMemoryRepository.getLawCaseByID(id);
     }
 
@@ -31,14 +56,16 @@ public class LawCaseServiceImpl implements LawCaseService {
         return inMemoryRepository.getAllLawCases();
     }
 
-    @Override
-    public LawCase updateLawCase(Integer id, LawCase LawCase) {
-        return inMemoryRepository.updateLawCase(id,LawCase);
-    }
+
 
     @Override
     public String deleteLawCaseByID(Integer id) {
-
+        try{
+            LawCase founded = inMemoryRepository.getLawCaseByID(id);
+        } catch (Exception e){
+            LOGGER.info("LawCase not found! id : {}", id.toString());
+            throw new LawCaseNotFoundException("LawCase with id: " + id + " was not found!");
+        }
         return inMemoryRepository.deleteById(id);
     }
 
