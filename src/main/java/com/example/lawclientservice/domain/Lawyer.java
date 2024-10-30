5e4adafc0b018ecc64656c2109143e41601a7fa6
PiotrHic <piotrhic@gmail.com>
2024-10-30 11:36:53 +0100

    Create Lawyer Class. Closes #54.

---
+++ src/main/java/com/example/lawclientservice/domain/Lawyer.java
@@ -0,0 +1,14 @@
+package com.example.lawclientservice.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Lawyer {
+
+    private Integer id;
+    private String name;
+}
