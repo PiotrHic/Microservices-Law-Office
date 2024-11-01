2e3f5a8076bad7d060d1a16f05e102fbdffd10c3
PiotrHic <piotrhic@gmail.com>
2024-11-02 00:53:45 +0100

    Set up Open API for Law Client Module. Closes #92.

--- src/main/java/com/example/lawclientservice/controller/LawClientController.java
+++ src/main/java/com/example/lawclientservice/controller/LawClientController.java
@@ -8,6 +8,10 @@ import com.example.lawclientservice.mapper.LawClientMapper;
 import com.example.lawclientservice.service.LawClientService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,6 +42,10 @@ public class LawClientController {
     private final String NUMBER_PATH = "{lawClientId}";
     private final String PATH_VARIABLE_PATH = "lawClientId";
 
+    @Operation(summary = "It adds a new Law Client to the database")
+    @ApiResponse(responseCode = "201",
+            description = "Add new Law Client to the database",
+            content = {@Content(mediaType =  "application/json")})
     @PostMapping
     ResponseEntity<LawClientDTO> createLawClient(@RequestBody LawClientDTO lawClientDTO){
         LawClient added = lawClientService.createLawClient(lawClientMapper.lawClientDTOToLawClient(lawClientDTO));
@@ -46,6 +54,15 @@ public class LawClientController {
                 HttpStatus.valueOf(201));
     }
 
+    @Operation(summary = "It updates Law Client with the new data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Update Law Client to the database",
+                    content = {@Content(mediaType =  "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Law Client was not found in database",
+                    content = {@Content(mediaType =  "application/json")}),
+    })
     @PutMapping(NUMBER_PATH)
     ResponseEntity <LawClientDTO> updateLawClientById(@PathVariable(PATH_VARIABLE_PATH) Integer lawClientId
             , @RequestBody  LawClientDTO lawClientDTO) {
@@ -55,6 +72,15 @@ public class LawClientController {
         return new ResponseEntity<>(lawClientMapper.lawClientToLawClientDTO(updated), HttpStatus.OK);
     }
 
+    @Operation(summary = "It brings one Law Client from the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Get one Law Client from the database",
+                    content = {@Content(mediaType =  "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Law Client was not found in database",
+                    content = {@Content(mediaType =  "application/json")}),
+    })
     @GetMapping(NUMBER_PATH)
     ResponseEntity <LawClientDTO> getLawClientById(@PathVariable(PATH_VARIABLE_PATH) Integer lawClientId) {
         LawClient founded = lawClientService.getLawClient(lawClientId);
@@ -62,6 +88,10 @@ public class LawClientController {
         return new ResponseEntity<>(lawClientMapper.lawClientToLawClientDTO(founded), HttpStatus.OK);
     }
 
+    @Operation(summary = "Takes all Law Clients from the database")
+    @ApiResponse(responseCode = "200",
+            description = "Gives all Law Clients from the database",
+            content = {@Content(mediaType =  "application/json")})
     @GetMapping
     ResponseEntity <List<LawClient>> getAllLawClients(){
         List<LawClient> LawClients = lawClientService
@@ -71,6 +101,10 @@ public class LawClientController {
         return new ResponseEntity<>(LawClients, HttpStatus.OK);
     }
 
+    @Operation(summary = "Deletes all Law Clients from the database")
+    @ApiResponse(responseCode = "200",
+            description = "Deletes all Law Clients from the database",
+            content = {@Content(mediaType =  "application/json")})
     @DeleteMapping(NUMBER_PATH)
     ResponseEntity <String> deleteLawClientById(@PathVariable(PATH_VARIABLE_PATH) Integer LawClientId){
         String deleted = lawClientService.deleteLawClientByID(LawClientId);
@@ -78,6 +112,10 @@ public class LawClientController {
         return new ResponseEntity<>(deleted, HttpStatus.OK);
     }
 
+    @Operation(summary = "Deletes all Law Clients from the database")
+    @ApiResponse(responseCode = "200",
+            description = "Deletes all Law Clients from the database",
+            content = {@Content(mediaType =  "application/json")})
     @DeleteMapping()
     ResponseEntity <String> deleteAllDancers(){
         lawClientService.deleteAllLawClients();
@@ -91,12 +129,10 @@ public class LawClientController {
         return "To jest test fallback";
     }
 
-    @GetMapping("forLawCase/{lawClientId}")
-    @CircuitBreaker(name = CB, fallbackMethod = "testFallBack")
-    public LawClient findLawClientByLawClientId(@PathVariable("lawClientId") Integer lawClientId){
-        return lawClientService.getLawClient(lawClientId);
-    };
-
+    @Operation(summary = "Request a list of Law Cases with Lawyer owned by the Law Client")
+    @ApiResponse(responseCode = "200",
+            description = "Request a list of Law Cases with Lawyer owned by the Law Client",
+            content = {@Content(mediaType =  "application/json")})
     @GetMapping("toBringLawCase-withLawyer/" + NUMBER_PATH)
     @CircuitBreaker(name = CB, fallbackMethod = "testFallBack")
     public LawClient findLawCaseWithLawyersByLawClientId(@PathVariable("lawClientId") Integer lawClientId){
@@ -105,6 +141,10 @@ public class LawClientController {
         return founded;
     };
 
+    @Operation(summary = "Request a list of Law Cases owned by the Law Client")
+    @ApiResponse(responseCode = "200",
+            description = "Request a list of Law Cases owned by the Law Client",
+            content = {@Content(mediaType =  "application/json")})
     @GetMapping("toBringLawCase/" + NUMBER_PATH)
     @CircuitBreaker(name = CB, fallbackMethod = "testFallBack")
     public LawClient findLawCaseByLawClientId(@PathVariable(PATH_VARIABLE_PATH) Integer lawClientId){
@@ -113,5 +153,14 @@ public class LawClientController {
         return founded;
     };
 
+    @Operation(summary = "Send a Law Client to the Law Case")
+    @ApiResponse(responseCode = "200",
+            description = "Send a Law Client to the Law Case",
+            content = {@Content(mediaType =  "application/json")})
+    @GetMapping("forLawCase/{lawClientId}")
+    @CircuitBreaker(name = CB, fallbackMethod = "testFallBack")
+    public LawClient findLawClientByLawClientId(@PathVariable("lawClientId") Integer lawClientId){
+        return lawClientService.getLawClient(lawClientId);
+    };
 
 }
